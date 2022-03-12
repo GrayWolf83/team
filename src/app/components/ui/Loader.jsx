f9aa0c22e3d4474c62b269b9599a9b3c6e0afe2a
Beshil <ersanbievbesh@gmail.com>
2022-03-12 12:00:58 +0300

    finished word for loader

---
+++ src/app/components/ui/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+const Loader = () => {
+    return (
+        <div className="loader-wrap">
+            <div className="lds-ripple">
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Loader
