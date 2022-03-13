baf49c700b5cd46bc3ae06ada333845b47b2fc49
Beshil <ersanbievbesh@gmail.com>
2022-03-13 14:19:41 +0300

    'fixed'

--- src/app/services/http.service.js
+++ src/app/services/http.service.js
@@ -1,12 +1,70 @@
 import axios from 'axios'
 import configFile from '../../config.json'
 
-console.log(configFile.apiEndpoint)
+const http = axios.create({
+    baseURL: configFile.apiEndpoint
+})
 
+http.interceptors.request.use(
+    async function (config) {
+        if (configFile.isFireBase) {
+            const containSlash = /\/$/gi.test(config.url)
+            config.url =
+                (containSlash ? config.url.slice(0, -1) : config.url) + '.json'
+            const expiresDate = localStorageService.getTokenExpiresDate()
+            const refreshToken = localStorageService.getRefreshToken()
+            if (refreshToken && expiresDate < Date.now()) {
+                const data = await authService.refresh()
+
+                localStorageService.setTokens({
+                    refreshToken: data.refresh_token,
+                    idToken: data.id_token,
+                    expiresIn: data.expires_id,
+                    localId: data.user_id
+                })
+            }
+            const accessToken = localStorageService.getAccessToken()
+            if (accessToken)
+                config.params = { ...config.params, auth: accessToken }
+        }
+        return config
+    },
+    function (error) {
+        return Promise.reject(error)
+    }
+)
+function transformData(data) {
+    return data && !data._id
+        ? Object.keys(data).map((key) => ({
+              ...data[key]
+          }))
+        : data
+}
+http.interceptors.response.use(
+    (res) => {
+        if (configFile.isFireBase)
+            res.data = { content: transformData(res.data) }
+
+        return res
+    },
+    function (error) {
+        const expectedErrors =
+            error.response &&
+            error.response.status >= 400 &&
+            error.response.status < 500
+
+        if (!expectedErrors) {
+            console.log(error)
+            toast.error('Something was wrong. Try it later')
+        }
+        return Promise.reject(error)
+    }
+)
 const httpService = {
-    get: axios.get(configFile.apiEndpoint),
-    post: axios.post(configFile.apiEndpoint),
-    put: axios.put(configFile.apiEndpoint),
-    delete: axios.delete(configFile.apiEndpoint)
+    get: http.get,
+    post: http.post,
+    put: http.put,
+    delete: http.delete,
+    patch: http.patch
 }
 export default httpService
--- src/app/services/user.service.js
+++ src/app/services/user.service.js
@@ -5,7 +5,6 @@ const userEndpoint = 'user/'
 const userService = {
     get: async () => {
         const { data } = await httpService.get(userEndpoint)
-
         return data
     }
 }
